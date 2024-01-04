8f780b997a452623a63ce461974a6c6a90ac9d67
Mikhail Kozlov <mikozlov@ptsecurity.com>
2024-01-04 17:27:04 +0300

    1 day

---
+++ d1_trebuchet/go.mod
@@ -0,0 +1,3 @@
+module d1_trebuchet
+
+go 1.21.5
--- d1_trebuchet/main.go
+++ d1_trebuchet/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"container/list"
 	"fmt"
 	"log"
 	"os"
@@ -10,6 +9,11 @@ import (
 )
 
 func main() {
+	partOne()
+	partTwo()
+}
+
+func partOne() {
 	file, err := os.Open("./input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -18,19 +22,95 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	// optionally, resize scanner's capacity for lines over 64K, see next example
-	count := 0
-	var intPointer *int
+	sum := 0
+
 	for scanner.Scan() {
-		intVar, _ := strconv.Atoi(scanner.Text())
-		if intPointer != nil && intVar > *intPointer {
-			count++
+		str := scanner.Text()
+		val1, val2 := 0, 0
+		for i := 0; i < len(str); i++ {
+			if str[i] >= '0' && str[i] <= '9' {
+				val1, _ = strconv.Atoi(string(str[i]))
+				break
+			}
 		}
-		intPointer = &intVar
+
+		for i := len(str) - 1; i >= 0; i-- {
+			if str[i] >= '0' && str[i] <= '9' {
+				val2, _ = strconv.Atoi(string(str[i]))
+				break
+			}
+		}
+
+		sum += val1*10 + val2
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(count)
+	fmt.Println("Part one: ", sum)
+}
+
+func MatchNumber(str string) int {
+	if str[0] >= '0' && str[0] <= '9' {
+		res, _ := strconv.Atoi(string(str[0]))
+		return res
+	}
+
+	numbers := [...]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	for i := 0; i < len(numbers); i++ {
+		num := numbers[i]
+		for j := 0; j < len(num); j++ {
+			if len(num) > len(str) || str[j] != num[j] {
+				break
+			}
+			if j == len(num)-1 {
+				return i + 1
+			}
+		}
+	}
+
+	return 0
+}
+
+func partTwo() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+
+	for scanner.Scan() {
+		str := scanner.Text()
+
+		val1, val2 := 0, 0
+		for i := 0; i < len(str); i++ {
+			val1 = MatchNumber(str[i:])
+			if val1 > 0 {
+				break
+			}
+		}
+
+		for i := len(str) - 1; i >= 0; i-- {
+			val2 = MatchNumber(str[i:])
+			if val2 > 0 {
+				break
+			}
+		}
+
+		res := val1*10 + val2
+		fmt.Println("Part two: ", res)
+
+		sum += res
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Part two: ", sum)
 }
